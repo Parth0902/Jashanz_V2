eac3da6588fb442277a778abff94fea1181150bb
Parth Godase <parthgodase123@gmail.com>
2024-06-06 00:06:06 +0530

    update: request refresh added

--- app/admin/_layout.jsx
+++ app/admin/_layout.jsx
@@ -87,7 +87,7 @@ export default function Layout() {
 
   useEffect(() => {
     fetchRequests();
-  }, [adminId, Token]);
+  }, [adminId, Token,refresh]);
 
   if (isLogout) {
     return <Redirect href={"/"} />;
